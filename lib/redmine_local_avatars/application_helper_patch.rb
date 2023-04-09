e53837a5a27231712c86577dbad99faeb388246f
Desertwalker <lakewalking@gmail.com>
2023-04-09 22:25:36 +0900

    helper update

--- lib/redmine_local_avatars/application_helper_patch.rb
+++ lib/redmine_local_avatars/application_helper_patch.rb
@@ -1,24 +1,23 @@
 module RedmineLocalAvatars
-    module ApplicationHelperPatch
-      def self.included(base) # :nodoc:
-        base.class_eval do
-          alias_method :avatar_without_local, :avatar
-          alias_method :avatar, :avatar_with_local
+  module ApplicationHelperPatch
+    extend ActiveSupport::Concern
+
+    included do
+      alias_method :avatar_without_local, :avatar
+      alias_method :avatar, :avatar_with_local
+    end
+
+    def avatar_with_local(user, options = {})
+      if user.is_a?(User)
+        av = user.attachments.find_by_description 'avatar'
+        if av
+          image_url = url_for :only_path => true, :controller => 'users', :action => 'get_avatar', :id => user
+          options[:size] = "64" unless options[:size]
+          title = "#{user.name}"
+          return "<img class=\"gravatar\" title=\"#{title}\" width=\"#{options[:size]}\" height=\"#{options[:size]}\" src=\"#{image_url}\" />".html_safe
         end
       end
-  
-      def avatar_with_local(user, options = {})
-        if user.is_a?(User)
-          av = user.attachments.find_by_description 'avatar'
-          if av
-            image_url = url_for :only_path => true, :controller => 'users', :action => 'get_avatar', :id => user
-            options[:size] = "64" unless options[:size]
-            title = "#{user.name}"
-            return "<img class=\"gravatar\" title=\"#{title}\" width=\"#{options[:size]}\" height=\"#{options[:size]}\" src=\"#{image_url}\" />".html_safe
-          end
-        end
-        avatar_without_local(user, options)
-      end
+      avatar_without_local(user, options)
     end
   end
-  
+end
